957e398cfa6b839ec5903ccfa3e8d30fdab07b87
sinscary <nbsharma@outlook.com>
2019-10-08 10:44:00 +0530

    add route to process rewards

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/process_rewards', to: 'rewards#process_rewards'
 end
